0e97ce2dcd902117c6f818658682461439f0ad14
angelDavid-alvaradoNina <drogonuel@gmail.com>
2025-06-14 13:38:09 -0400

    Actualizar HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -7,6 +7,6 @@ package holamundo;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("¡Hola Mundo desde Java en NetBeans!");
+        System.out.println("¡Hola Mundo cruel!");
     }
 }
